59d8d8616811ab8a6eee65de76d0fb77cdb05217
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-26 14:07:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+var getFirstSelector = (selector) =>{
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
+}
